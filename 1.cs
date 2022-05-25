62aa69e20395aeed20aa01e28ba98c14cb90a5d7
Fateh Mammadli <fatehmmmdli@Fatehs-MacBook-Air.local>
2022-05-25 10:44:18 +0400

    ljsdlf;jkasnldkasjd

---
+++ 1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace sqdasdasd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cuce = Console.ReadLine();
+            int num = Convert.ToInt32(cuce);
+            Console.WriteLine(Sum(num));
+        }
+        
+        static int Sum(int number)
+        {
+            int sumOfNumbers = 0;
+            while (number!=0)
+            {
+                int lastDigit = number % 10;
+                sumOfNumbers += lastDigit;
+                number = (number - number % 10) / 10;
+                
+            }
+
+            return sumOfNumbers;
+        }
+
+
+
+
+    }
+}
